1a32b1c0733f39ac33e838bb27025c6b311c8da9
Elikem <juniormedehou@gmail.com>
2025-02-21 14:00:21 +0100

    hotfix: fix firebase config

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -20,32 +20,32 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDf2tFDkkZ6I4GMx8Dplujb3yGglP02AXE',
-    appId: '1:192468342798:web:e063f16e97a4410ece445f',
+    apiKey: 'AIzaSyBUbB-h-kMscMAuLYSqGxhl2L4m-ifZalw',
+    appId: '1:192468342798:web:53408642c585095bce445f',
     messagingSenderId: '192468342798',
     projectId: 'hookup4u-6be59',
     authDomain: 'hookup4u-6be59.firebaseapp.com',
-    storageBucket: 'hookup4u-6be59.appspot.com',
-    measurementId: 'G-6BE59',
-    androidClientId: '192468342798-00000000000000000000000000000000.apps.googleusercontent.com',
-    iosClientId: '192468342798-00000000000000000000000000000000.apps.googleusercontent.com',
-    iosBundleId: 'com.example.hookup4u',
+    storageBucket: 'hookup4u-6be59.firebasestorage.app',
+    measurementId: 'G-FY0CFYBJL0',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDf2tFDkkZ6I4GMx8Dplujb3yGglP02AXE',
-    appId: '1:192468342798:android:e063f16e97a4410ece445f',
+    appId: '1:192468342798:android:52361d732c264974ce445f',
     messagingSenderId: '192468342798',
     projectId: 'hookup4u-6be59',
     storageBucket: 'hookup4u-6be59.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'YOUR-IOS-API-KEY',
-    appId: 'YOUR-IOS-APP-ID',
-    messagingSenderId: 'YOUR-SENDER-ID',
-    projectId: 'YOUR-PROJECT-ID',
-    storageBucket: 'YOUR-STORAGE-BUCKET',
-    iosBundleId: 'YOUR-IOS-BUNDLE-ID',
+    apiKey: 'AIzaSyD0dEyF7_bVTFPkJOZDwOVpn1Ikiwbw5oI',
+    appId: '1:192468342798:ios:ac90a20e9cfba3e6ce445f',
+    messagingSenderId: '192468342798',
+    projectId: 'hookup4u-6be59',
+    storageBucket: 'hookup4u-6be59.firebasestorage.app',
+    androidClientId: '192468342798-iprs8pp3neij56rqdg2q86d085gaqbbc.apps.googleusercontent.com',
+    iosClientId: '192468342798-o5e9nsdf6q2d6ma43is8vt96amm3fauu.apps.googleusercontent.com',
+    iosBundleId: 'com.soloparent.app',
   );
+
 }
